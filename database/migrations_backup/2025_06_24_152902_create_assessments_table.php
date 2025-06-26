54c2dc2cf66d59f301fde41b62e105dd54eed15d
Juniyasyos <juniyasyos@gmail.com>
2025-06-26 21:33:18 +0700

    rename and ordering migration file

---
+++ database/migrations_backup/2025_06_24_152902_create_assessments_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('assessments', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('course_id')->constrained()->onDelete('cascade');
+            $table->string('title');
+            $table->text('description')->nullable();
+            $table->enum('type', ['pre', 'post', 'quiz'])->default('quiz');
+            $table->integer('duration_minutes')->nullable(); // opsional durasi pengerjaan
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('assessments');
+    }
+};
---
+++ database/migrations_backup/2025_06_24_152903_create_assessment_attempts_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('assessment_attempts', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('assessment_id')->constrained()->onDelete('cascade');
+            $table->foreignId('user_id')->constrained()->onDelete('cascade');
+
+            $table->integer('score')->nullable(); 
+            $table->integer('max_score')->nullable(); // maksimal skor
+            $table->json('answers')->nullable(); // jawaban user (bisa json: question_id => answer)
+            $table->timestamp('started_at')->nullable();
+            $table->timestamp('completed_at')->nullable();
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('assessment_attempts');
+    }
+};
